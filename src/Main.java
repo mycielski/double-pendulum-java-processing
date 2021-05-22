f36d65d8a07d69d7809180b7c090c9cb201bf78f
Mycielski <76878490+Mycielski@users.noreply.github.com>
2021-05-22 20:07:49 +0200

    added clarifying comments in the code

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main extends PApplet {
     float w1 = 0, w2 = 0;   // initial velocities of the pendulums' pendants
     float e1 = 0, e2 = 0;   // initial accelerations of the pendulums' pendants
 
-    final float G = 1;      // gravity constant
+    final float G = 0.981f;      // gravity constant
 
     /**
      * Main method.
@@ -56,9 +56,12 @@ public class Main extends PApplet {
      */
     public void draw() {
 
-        e1 = (-G * (2 * m1 + m2) * sin(theta1) - m2 * G * sin(theta1 - 2 * theta2) - 2 * sin(theta1 - theta2) * m2 * (w2 * w2 * r2 + w1 * w1 * r1 * cos(theta1 - theta2))) / (r1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
-        e2 = (2 * sin(theta1 - theta2) * (w1 * w1 * r1 * (m1 + m2) + G * (m1 + m2) * cos(theta1) + w2 * w2 + r2 * m2 * cos(theta1 - theta2))) / (r2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
+        // Calculating angular accelerations in one step, like below, is too fast for processing's renderer.
+        // Due to that, the program is slowed down by performing the necessary calculation in multiple steps.
+        // e1 = (-G * (2 * m1 + m2) * sin(theta1) - m2 * G * sin(theta1 - 2 * theta2) - 2 * sin(theta1 - theta2) * m2 * (w2 * w2 * r2 + w1 * w1 * r1 * cos(theta1 - theta2))) / (r1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
+        // e2 = (2 * sin(theta1 - theta2) * (w1 * w1 * r1 * (m1 + m2) + G * (m1 + m2) * cos(theta1) + w2 * w2 + r2 * m2 * cos(theta1 - theta2))) / (r2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2)));
 
+        // Angular acceleration of the 1st pendulum
         float factor1 = -G * (2 * m1 + m2) * sin(theta1);
         float factor2 = -m2 * G * sin(theta1 - 2 * theta2);
         float factor3 = -2 * sin(theta1 - theta2) * m2;
@@ -66,6 +69,7 @@ public class Main extends PApplet {
         float denominator = r1 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2));
         float e1 = (factor1 + factor2 + factor3 * factor4) / denominator;
 
+        // Angular acceleration of the 2nd pendulum
         factor1 = 2 * sin(theta1 - theta2);
         factor2 = (w1 * w1 * r1 * (m1 + m2));
         factor3 = G * (m1 + m2) * cos(theta1);
@@ -73,33 +77,44 @@ public class Main extends PApplet {
         denominator = r2 * (2 * m1 + m2 - m2 * cos(2 * theta1 - 2 * theta2));
         float e2 = (factor1 * (factor2 + factor3 + factor4)) / denominator;
 
+        // Update angular velocity of the pendulums
+        w1 += e1;
+        w2 += e2;
+
+        // Dampening (optional)
+        w1 = 0.9999f * w1;
+        w2 = 0.9999f * w2;
+
+        // Update the angles of the pendulums
+        theta1 += w1;
+        theta2 += w2;
+
+        // Calculate the cartesian coordinates of 1st pendulum's pendant
         float x1 = r1 * sin(theta1);
-        float y1 = r1 * cos(theta1); // Cartesian coordinates of the first pendulum's pendant
+        float y1 = r1 * cos(theta1);
 
+        // Calculate the cartesian coordinates of 2nd pendulum's pendant
         float x2 = x1 + r2 * sin(theta2);
-        float y2 = y1 + r2 * cos(theta2); // Cartesian coordinates of the second pendulum's pendant
+        float y2 = y1 + r2 * cos(theta2);
 
+        // Move the (0, 0) point to the center of the window
         translate(xOffset, yOffset);
 
+        // Set the background to be white, the drawing to be black and its lines 3px wide.
         background(255);
         stroke(0);
         strokeWeight(3);
         fill(0);
 
+        // Draw the 1st pendulum's radius and its pendant.
+        // The pendant is drawn proportional to its mass
         line(0, 0, x1, y1);
         ellipse(x1, y1, m1, m1);
 
+        // Draw the 2nd pendulum's radius and its pendant
+        // The pendant is drawn proportional to its mass
         line(x1, y1, x2, y2);
         ellipse(x2, y2, m2, m2);
-
-        theta1 += w1;
-        theta2 += w2;
-        w1 += e1;
-        w2 += e2;
-
-        // Dampening
-        w1 = 0.99f * w1;
-        w2 = 0.99f * w2;
 
     }
 
